2e02e58c7272e20b37d3ba423d9a7b41ad1e2410
ShinHyeJi <80444987+hyeji2021@users.noreply.github.com>
2023-04-03 11:51:31 +0900

    Update bubbleSort.cpp

--- 신혜지/2장.분할정복/bubbleSort.cpp
+++ 신혜지/2장.분할정복/bubbleSort.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
 using namespace std;
+int bubbleSort(int* p, int size);
 
 int main() {
-  return 0;
+  int array[500];
+	int size;
+	cin >> size;
+	int swapNum = 0;
+
+	for (int i = 0; i < size; i++) {
+		cin >> array[i];
+	}
+
+	cout << bubbleSort(array, size);
+	return 0;
 }
+
+int bubbleSort(int* p, int size) {
+	int num = 0;
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size - 1; j++) {
+			if (p[j] > p[j + 1]) {
+				int temp = p[j];
+				p[j] = p[j + 1];
+				p[j + 1] = temp;
+				num++;
+			}
+		}
+	}
+	return num;
+} // 마음에 들지 않는 방법
